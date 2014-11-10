34415ffb8f9bf9ba8ae69acb7edc39d429c2f9bc
kiriakosv <kir.velissariou@gmail.com>
2014-11-10 22:25:12 +0200

    minor modifications

--- happy.py
+++ happy.py
@@ -1,20 +1,22 @@
-numbers_found = 0
-happy_numbers = []
-i = 1
+numbers_found = 1
+happy_numbers = [1]
+i = 2
 while numbers_found != 8:
     j = i
-    print "i = ", i
     string_number = str(i)
     sum_of_squares = 0
+    memory = []
+    memory.append(i)
     while True:
         for k in string_number:
             sum_of_squares += int(k) ** 2
-        if sum_of_squares == i and i != 1:
+        if sum_of_squares in memory:
             break
         if sum_of_squares == 1:
             happy_numbers.append(i)
             numbers_found += 1
             break
+        memory.append(sum_of_squares)
         string_number = str(sum_of_squares)
         sum_of_squares = 0
     i += 1
